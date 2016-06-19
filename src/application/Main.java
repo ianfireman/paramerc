b2d26d2d3d3fc019b272ff8e415081f6b5e1201c
Abhner Araújo <afga@cin.ufpe.br>
2016-06-19 19:56:16 -0300

    Visão do login e do menu, além da configuraçao inicial

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,23 +1,69 @@
 package application;
 	
+import javafx.geometry.Rectangle2D;
+
+import java.io.IOException;
+
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 
 
 public class Main extends Application {
+	
+	private BorderPane rootLayout;
+	private Stage primaryStage;
 	@Override
 	public void start(Stage primaryStage) {
 		try {
+			this.primaryStage  = primaryStage;
 			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
-			primaryStage.show();
+			Rectangle2D primaryScreenBounds = Screen.getPrimary().getVisualBounds();
+	        //set Stage boundaries to visible bounds of the main screen
+			primaryStage.setX(primaryScreenBounds.getMinX());
+			primaryStage.setY(primaryScreenBounds.getMinY());
+			primaryStage.setWidth(primaryScreenBounds.getWidth()*0.2);
+			primaryStage.setHeight(primaryScreenBounds.getHeight());
+			System.out.println(primaryScreenBounds.getWidth()*0.2);
+			System.out.println(primaryScreenBounds.getHeight());
+			
+			initRootLayout();
+
+			showLoginMenu();
+
+			
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
+	}
+	
+	public void showLoginMenu(){
+		try{
+			FXMLLoader loader = new FXMLLoader();
+	        loader.setLocation(Main.class.getResource("../view/Vw_Login.fxml"));
+	        AnchorPane login = (AnchorPane) loader.load();
+	        rootLayout.setCenter(login);	
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+		
+	}
+	
+	public void initRootLayout(){
+		try{
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(Main.class.getResource("../views/RootLayout.fxml"));
+			rootLayout = (BorderPane) loader.load();
+			Scene scene = new Scene(rootLayout);
+			primaryStage.setScene(scene);
+            primaryStage.show();
+		} catch (IOException e) {
+            e.printStackTrace();
+        }
 	}
 	
 	public static void main(String[] args) {
