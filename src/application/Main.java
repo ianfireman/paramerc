fb6d2f577d04247d6c59cddd2d9c676bf7665771
imlbf <imlbf@cin.ufpe.br>
2016-06-25 01:12:06 -0300

    sessions working(not db connecting)

--- src/application/Main.java
+++ src/application/Main.java
@@ -15,7 +15,7 @@ import javafx.scene.layout.BorderPane;
 
 public class Main extends Application {
 	
-	private BorderPane rootLayout;
+	public static BorderPane rootLayout;
 	private Stage primaryStage;
 	@Override
 	public void start(Stage primaryStage) {
@@ -41,7 +41,7 @@ public class Main extends Application {
 	public void showLoginMenu(){
 		try{
 			FXMLLoader loader = new FXMLLoader();
-	        loader.setLocation(Main.class.getResource("../views/Vw_Login.fxml"));
+	        loader.setLocation(Main.class.getResource("../view/Vw_Login.fxml"));
 	        AnchorPane login = (AnchorPane) loader.load();
 	        rootLayout.setCenter(login);	
 		}catch(Exception e){
---
+++ src/application/MainController.java
@@ -0,0 +1,50 @@
+package application;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import exceptions.Creation_Exception;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import models.Session;
+
+public class MainController implements Initializable {
+	
+	public static Session currentUser;
+	
+	@FXML
+	private TextField loginField;
+	@FXML
+	private PasswordField passwordField;
+	@FXML
+	private Label loginAlert;
+	
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		// TODO Auto-generated method stub
+	}
+	
+	public void loginUser(ActionEvent e){
+		try {
+			currentUser = new Session(loginField.getText(), passwordField.getText());
+			FXMLLoader loader = new FXMLLoader();
+	        loader.setLocation(Main.class.getResource("../view/Vw_Menu.fxml"));
+	        AnchorPane menu = (AnchorPane) loader.load();
+	        Main.rootLayout.setCenter(menu);			
+		} catch (Creation_Exception e1) {
+			// TODO Auto-generated catch block
+			loginAlert.setText(e1.getErrors().toString());
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			System.out.println("erro IOEx");
+		}
+	}
+
+}
