90b0d80096f9fa9f863ebc4c1d7a4379ef03c2ed
imlbf <imlbf@cin.ufpe.br>
2016-06-25 20:14:02 -0300

    check if already in a module

--- src/controllers/MenuController.java
+++ src/controllers/MenuController.java
@@ -36,27 +36,45 @@ public class MenuController implements Initializable {
 	}	
 	
 	public void startCaixa() throws IOException {
-		stage.hide();
-		parent = FXMLLoader.load(getClass().getResource("../view/Vw_caixa.fxml"));
-		scene = new Scene(parent);
-		stage.setScene(scene);
-		stage.show();		
+		if(!checkModule("Caixa")){ // se o modulo nao for Caixa entao ele abre o Caixa
+			stage.hide();
+			parent = FXMLLoader.load(getClass().getResource("../view/Vw_caixa.fxml"));
+			scene = new Scene(parent);
+			stage.setScene(scene);
+			stage.setTitle("Caixa");
+			stage.show();
+		}
+	}
+
+	public void startEstoque() throws IOException{
+		if(!checkModule("Estoque")){ // se o modulo nao for estoque entao ele abre o estoque
+			stage.hide();
+			parent = FXMLLoader.load(getClass().getResource("../view/Vw_caixa.fxml"));
+			scene = new Scene(parent);
+			stage.setScene(scene);
+			stage.setTitle("Estoque");
+			stage.show();	
+		}
 	}
 	
-	public void startEstoque(){
-		stage.hide();
-		//parent = FXMLLoader.load(getClass().getResource("../view/Vw_estoque.fxml"));
-		//scene = new Scene(parent);
-		//stage.setScene(scene);
-		//stage.show();	
+	public void startFinanceiro() throws IOException{
+		if(!checkModule("Financeiro")){ // se o modulo nao for Financeiro entao ele abre o Financeiro
+			stage.hide();
+			parent = FXMLLoader.load(getClass().getResource("../view/Vw_caixa.fxml"));
+			scene = new Scene(parent);
+			stage.setScene(scene);
+			stage.setTitle("Financeiro");
+			stage.show();	
+		}
 	}
 	
-	public void startFinanceiro(){
-		stage.hide();
-		//parent = FXMLLoader.load(getClass().getResource("../view/Vw_financeiro.fxml"));
-		//scene = new Scene(parent);
-		//stage.setScene(scene);
-		//stage.show();	
+	private boolean checkModule(String title) {
+		if(scene == null){
+			return false;
+		} else if(stage.getTitle().equals(title)){
+			return true;
+		}
+		return false;
 	}
 	
 	public void logoutUser(ActionEvent e){
