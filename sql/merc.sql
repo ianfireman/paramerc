38e4301c9d26ac06c8df841b538513e66bd4fa79
Anderson Túlio Pinho de Siqueira <djtulioo@gmail.com>
2016-06-28 17:25:38 -0300

    adicionando estrutura sql e mwb

---
+++ sql/merc.sql
@@ -0,0 +1,112 @@
+# ************************************************************
+# Sequel Pro SQL dump
+# Version 4096
+#
+# http://www.sequelpro.com/
+# http://code.google.com/p/sequel-pro/
+#
+# Host: 127.0.0.1 (MySQL 5.6.24)
+# Database: merc
+# Generation Time: 2016-06-28 20:24:27 +0000
+# ************************************************************
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+
+# Dump of table cliente
+# ------------------------------------------------------------
+
+CREATE TABLE `cliente` (
+  `id` int(11) NOT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  `nome` varchar(50) DEFAULT NULL,
+  `cpf` varchar(15) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+
+# Dump of table item
+# ------------------------------------------------------------
+
+CREATE TABLE `item` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `created_at` datetime DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  `produto_id` int(11) NOT NULL,
+  `quantidade` int(11) DEFAULT NULL,
+  `valor` float DEFAULT NULL,
+  `pedido_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_item_produto_idx` (`produto_id`),
+  KEY `fk_item_pedido1_idx` (`pedido_id`),
+  CONSTRAINT `fk_item_pedido1` FOREIGN KEY (`pedido_id`) REFERENCES `pedido` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_item_produto` FOREIGN KEY (`produto_id`) REFERENCES `produto` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+
+# Dump of table pedido
+# ------------------------------------------------------------
+
+CREATE TABLE `pedido` (
+  `id` int(11) NOT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  `cliente_id` int(11) NOT NULL,
+  `vendedor_id` int(11) NOT NULL,
+  `valor` float DEFAULT NULL,
+  `nota_fiscal` varchar(50) DEFAULT NULL,
+  `forma_pagamento` enum('dinheiro','cartao','boleto','cheque') DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_pedido_cliente1_idx` (`cliente_id`),
+  KEY `fk_pedido_vendedor1_idx` (`vendedor_id`),
+  CONSTRAINT `fk_pedido_cliente1` FOREIGN KEY (`cliente_id`) REFERENCES `cliente` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_pedido_vendedor1` FOREIGN KEY (`vendedor_id`) REFERENCES `vendedor` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+
+# Dump of table produto
+# ------------------------------------------------------------
+
+CREATE TABLE `produto` (
+  `id` int(11) NOT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  `nome` varchar(50) DEFAULT NULL,
+  `quantidade` int(11) DEFAULT NULL,
+  `valor` float DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+
+# Dump of table vendedor
+# ------------------------------------------------------------
+
+CREATE TABLE `vendedor` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `created_at` datetime DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  `nome` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+
+
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
